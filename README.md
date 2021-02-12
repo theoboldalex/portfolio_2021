085466f75eeec5ad5460f6801f82cfb142eadc24
theoboldalex <theoboldalex@gmail.com>
2021-02-12 22:55:06 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Alex Theobold - Portfolio 2021
+
+This is the repo for my portfolio website built lovingly with [Nuxt](https://nuxtjs.org/).
+
+
---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="container">
+    <h1 class="text-bold text-5xl p-8">Hello from Wales! 🏴󠁧󠁢󠁷󠁬󠁳󠁿</h1>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
